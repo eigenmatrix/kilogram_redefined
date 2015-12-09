869711e9f5cedc68b4d2d7e70ce7ac7273e0b4e1
malex <malex.y@gmail.com>
2015-12-08 23:00:25 -0500

    Converting to python3.0
    Manually...because changes are few.

--- google-python-exercises/basic/list1.py
+++ google-python-exercises/basic/list1.py
@@ -50,22 +50,21 @@ def front_x(words):
   x += o
   return x
 
-
-
 # C. sort_last
 # Given a list of non-empty tuples, return a list sorted in increasing
 # order by the last element in each tuple.
 # e.g. [(1, 7), (1, 3), (3, 4, 5), (2, 2)] yields
 # [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
 # Hint: use a custom key= function to extract the last element form each tuple.
+import functools
 def sort_last(tuples):
   # +++your code here+++
   def cmpa(a,b):
     if a[-1] > b[-1]:
       return 1
     return -1
-
-  return sorted(tuples, cmp=cmpa)
+  # python3.0 compatible requires key instead of a cmp argument  
+  return sorted(tuples, key = functools.cmp_to_key(cmpa))
 
 
 # Simple provided test() function used in main() to print
@@ -75,18 +74,15 @@ def test(got, expected):
     prefix = ' OK '
   else:
     prefix = '  X '
-  print '%s got: %s expected: %s' % (prefix, repr(got), repr(expected))
+  print("%s got: %s expected: %s" % (prefix, repr(got), repr(expected)))
 
 
 # Calls the above functions with interesting inputs.
 def main():
-  print 'match_ends'
   test(match_ends(['aba', 'xyz', 'aa', 'x', 'bbb']), 3)
   test(match_ends(['', 'x', 'xy', 'xyx', 'xx']), 2)
   test(match_ends(['aaa', 'be', 'abc', 'hello']), 1)
 
-  print
-  print 'front_x'
   test(front_x(['bbb', 'ccc', 'axx', 'xzz', 'xaa']),
        ['xaa', 'xzz', 'axx', 'bbb', 'ccc'])
   test(front_x(['ccc', 'bbb', 'aaa', 'xcc', 'xaa']),
@@ -94,9 +90,6 @@ def main():
   test(front_x(['mix', 'xyz', 'apple', 'xanadu', 'aardvark']),
        ['xanadu', 'xyz', 'aardvark', 'apple', 'mix'])
 
-       
-  print
-  print 'sort_last'
   test(sort_last([(1, 3), (3, 2), (2, 1)]),
        [(2, 1), (3, 2), (1, 3)])
   test(sort_last([(2, 3), (1, 2), (3, 1)]),
